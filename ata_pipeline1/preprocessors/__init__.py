177b6bdbfd9c0666cb2e4cc1ee2f1f0356a6f118
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-01-16 15:14:34 +0700

    [change] have notebook import preprocessors directly from __init__.py

---
+++ ata_pipeline1/preprocessors/__init__.py
@@ -0,0 +1,9 @@
+from .aggregate import AggregatePageActivities
+from .non_aggregate import (
+    AddFieldDeviceIsMobile,
+    AddFieldEventParentId,
+    AddFieldFormSubmitIsNewsletter,
+    AddFieldMaxScrollDepth,
+    ConvertFieldTypes,
+    SortFieldTimestamp,
+)
