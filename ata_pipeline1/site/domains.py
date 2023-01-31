6ba89845e8c39c7e15f184baa6f21617a9fd1750
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-01-31 15:06:33 -0500

    [add] domains

---
+++ ata_pipeline1/site/domains.py
@@ -0,0 +1,4 @@
+DOMAINS_AFRO_LA = ["afrolanews.org", "afrolanews.beehiiv.com"]
+DOMAINS_DALLAS_FREE_PRESS = ["dallasfreepress.com"]
+DOMAINS_OPEN_VALLJO = ["openvallejo.org"]
+DOMAINS_THE_19TH = ["19thnews.org"]
--- ata_pipeline1/site/sites.py
+++ ata_pipeline1/site/sites.py
@@ -1,5 +1,12 @@
 from dataclasses import dataclass
+from typing import List
 
+from ata_pipeline1.site.domains import (
+    DOMAINS_AFRO_LA,
+    DOMAINS_DALLAS_FREE_PRESS,
+    DOMAINS_OPEN_VALLJO,
+    DOMAINS_THE_19TH,
+)
 from ata_pipeline1.site.names import SiteName
 from ata_pipeline1.site.newsletter import (
     NEWSLETTER_SIGNUP_VALIDATOR_AFRO_LA,
@@ -24,6 +31,7 @@ class Site:
     """
 
     name: SiteName
+    domains: List[str]  # Could be something strictier than str if we reallly need
     newsletter_signup_validator: SiteNewsletterSignupValidator
     page_type_classifier: SitePageClassifier
 
@@ -31,21 +39,25 @@ class Site:
 # ---------- SITE OBJECTS ----------
 AFRO_LA = Site(
     name=SiteName.AFRO_LA,
+    domains=DOMAINS_AFRO_LA,
     newsletter_signup_validator=NEWSLETTER_SIGNUP_VALIDATOR_AFRO_LA,
     page_type_classifier=PAGE_CLASSIFIER_AFRO_LA,
 )
 DALLAS_FREE_PRESS = Site(
     name=SiteName.DALLAS_FREE_PRESS,
+    domains=DOMAINS_DALLAS_FREE_PRESS,
     newsletter_signup_validator=NEWSLETTER_SIGNUP_VALIDATOR_DALLAS_FREE_PRESS,
     page_type_classifier=PAGE_CLASSIFIER_DALLAS_FREE_PRESS,
 )
 OPEN_VALLEJO = Site(
     name=SiteName.OPEN_VALLEJO,
+    domains=DOMAINS_OPEN_VALLJO,
     newsletter_signup_validator=NEWSLETTER_SIGNUP_VALIDATOR_OPEN_VALLEJO,
     page_type_classifier=PAGE_CLASSIFIER_OPEN_VALLEJO,
 )
 THE_19TH = Site(
     name=SiteName.THE_19TH,
+    domains=DOMAINS_THE_19TH,
     newsletter_signup_validator=NEWSLETTER_SIGNUP_VALIDATOR_THE_19TH,
     page_type_classifier=PAGE_CLASSIFIER_THE_19TH,
 )
