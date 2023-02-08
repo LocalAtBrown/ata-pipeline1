ae710030cab803c0b4f2043881ae25d5e379b59b
Raaid <raaid@protonmail.com>
2023-02-08 12:19:33 -0500

    add alt_fetch_events (#8)
    * add alt_fetch_events
    
    * remove print, rename variable
    
    * change right hand bound to be non inclusive

--- ata_pipeline1/fetch_events.py
+++ ata_pipeline1/fetch_events.py
@@ -5,6 +5,8 @@ import pandas as pd
 from ata_db_models.models import Event
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.functions import max as sqlmax
+from sqlalchemy.sql.functions import min as sqlmin
 from sqlmodel import select
 
 from ata_pipeline1.helpers.enums import EventName, SiteName
@@ -29,4 +31,39 @@ def fetch_events(
         data = [event[0].dict() for event in results]
 
     # convert to dataframe
+    return pd.DataFrame(data=data)
+
+
+def alt_fetch_events(site_name: SiteName, start: datetime, end: datetime, engine: Engine) -> pd.DataFrame:
+    """
+    Grabs event data from postgres for a given site and datetime range. Gets all sessions that start during or after
+    the start time and end by the end time.
+    """
+    with Session(engine) as session:
+        statement1 = (
+            select(
+                Event.domain_userid,
+                sqlmin(Event.domain_sessionidx).label("minidx"),
+                sqlmax(Event.domain_sessionidx).label("maxidx"),
+            )
+            .where(Event.site_name == site_name, Event.derived_tstamp >= start, Event.derived_tstamp < end)
+            .group_by(Event.domain_userid)
+        )
+        sub1 = statement1.subquery("s1")
+        joined = (
+            select(Event)
+            .join(
+                sub1,
+                (Event.domain_userid == sub1.c.domain_userid)
+                & (Event.domain_sessionidx >= sub1.c.minidx)
+                & (Event.domain_sessionidx < sub1.c.maxidx),
+            )
+            .where(
+                Event.site_name == site_name,
+            )
+            .order_by(Event.derived_tstamp)
+        )
+        results = session.execute(joined)
+        data = [event[0].dict() for event in results]
+
     return pd.DataFrame(data=data)
