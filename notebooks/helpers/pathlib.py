ad6d795795b8c476594e6f35797ee23053601262
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-02-09 11:47:17 -0500

    [change] move current checkpoints to new dir corresponding to csv prefix

--- notebooks/helpers/pathlib.py
+++ notebooks/helpers/pathlib.py
@@ -8,11 +8,11 @@ from ata_pipeline1.site.names import SiteName
 PATH_DATA = Path(os.getcwd()) / "data"
 
 
-def get_path_site_checkpoint(checkpoint: int, site: SiteName) -> Path:
+def get_path_site_checkpoint(csv_prefix: str, checkpoint: int, site: SiteName) -> Path:
     """
     Grabs a site's checkpoint pickled DataFrame.
     """
-    path_parent = PATH_DATA / "checkpoints" / str(checkpoint)
+    path_parent = PATH_DATA / f"{csv_prefix}_checkpoints" / str(checkpoint)
     # Create parent if it doesn't exist, ignore otherwise
     path_parent.mkdir(parents=True, exist_ok=True)
 
